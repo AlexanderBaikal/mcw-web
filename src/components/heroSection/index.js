26212efb0670351ed856b18abea0e1316d787250
AlexanderBaikal <abaikaler@mail.ru>
2022-04-13 14:21:53 -0500

    fixed hero

--- src/components/heroSection/index.js
+++ src/components/heroSection/index.js
@@ -3,32 +3,36 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import React from "react"
 import { Button } from "../ButtonElements"
 import "./HeroSection.css"
-import image from "../../images/hand.png"
+import image from "../../images/handbg.jpg"
 
 const HeroSection = () => {
   return (
-    <div className="hero-container">
-      <div className="hero-left">
-        <div className="title">
-          <p>Car wash</p>
-          <p>in an instant</p>
+    <div className="hero-wrapper">
+      <div className="hero-container">
+        <div className="hero-left-wrapper">
+          <div className="hero-left">
+            <div className="title">
+              <p>Car wash</p>
+              <p>in an instant</p>
+            </div>
+            <div className="subtitle">
+              <p>No more long lines.</p>
+              <p>just sign up and come</p>
+            </div>
+            <div className="hero-btns">
+              <Button fontBig primary>
+                Get started
+              </Button>
+            </div>
+            <div className="subtitle">
+              <p>Additional.</p>
+              <p>information</p>
+            </div>
+          </div>
         </div>
-        <div className="subtitle">
-          <p>No more long lines.</p>
-          <p>just sign up and come</p>
+        <div className="hero-right">
+          <img className="image-right" src={image} alt={"hand"} />
         </div>
-        <div className="hero-btns">
-          <Button fontBig primary>
-            Get started
-          </Button>
-        </div>
-        <div className="subtitle">
-          <p>Additional.</p>
-          <p>information</p>
-        </div>
-      </div>
-      <div className="hero-right">
-        <img className="image-right" src={image} alt={"hand"} />
       </div>
     </div>
   )
